b2cab62ddb8a7947cba773e86e5faaf13af9d6d8
schuhmaj <jonas.schuhmacher@tum.de>
2022-01-17 23:27:07 +0100

    renamed variable

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char *argv[]) {
     SPDLOG_INFO("The answer to your question is 42!");
 
     std::shared_ptr<ConfigSource> config = std::make_shared<YAMLConfigReader>("../example-config/example.yaml");
-    auto points = config->getDataSource()->getPolyhedron();
+    auto poly = config->getDataSource()->getPolyhedron();
 
     return 0;
 }
