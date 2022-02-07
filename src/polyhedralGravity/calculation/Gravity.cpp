fdc01092e509741e0958836598cf0ee024332431
Jonas Schuhmacher <jonas.schuhmacher@tum.de>
2022-02-07 16:41:53 +0100

    const
    Co-authored-by: FG-TUM <FG-TUM@users.noreply.github.com>

--- src/polyhedralGravity/calculation/Gravity.cpp
+++ src/polyhedralGravity/calculation/Gravity.cpp
@@ -9,10 +9,10 @@ std::vector<std::array<std::array<double, 3>, 3>> Gravity::calculateGij() {
     using ::util::operator-;
     std::vector<std::array<std::array<double, 3>, 3>> g;
     g.reserve(_polyhedron.size());
-    for (auto &face: _polyhedron.getFaces()) {
-        auto &node0 = _polyhedron.getNode(face[0]);
-        auto &node1 = _polyhedron.getNode(face[1]);
-        auto &node2 = _polyhedron.getNode(face[2]);
+    for (const auto &face: _polyhedron.getFaces()) {
+        const auto &node0 = _polyhedron.getNode(face[0]);
+        const auto &node1 = _polyhedron.getNode(face[1]);
+        const auto &node2 = _polyhedron.getNode(face[2]);
         g.push_back({
                             node1 - node0,
                             node2 - node1,
