e0cf68809c7bbb4e0004a68a665a7c1c8f8c7ac2
Jonas Schuhmacher <jonas.schuhmacher@tum.de>
2022-02-07 18:09:43 +0100

    gravityConstant update
    Co-authored-by: Pablo Gómez <pablo.gomez@gmx.de>

--- src/polyhedralGravity/util/UtilityConstants.h
+++ src/polyhedralGravity/util/UtilityConstants.h
@@ -6,7 +6,7 @@ namespace polyhedralGravity::util {
      * The gravitational constant G in [m^3/(kg*s^2)].
      * @related in his paper above Equation (4)
      */
-    constexpr double gravitationalConstant = 6.67259e-11;
+    constexpr double gravitationalConstant = 6.67430e-11;
 
     /**
      * The assumed constant density rho for a polyhedron after Tsoulis paper in [kg/m^3].
