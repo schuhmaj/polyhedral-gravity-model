52616ff80fb07d5c19c3035ba118684cd54776de
Jonas Schuhmacher <jonas.schuhmacher@tum.de>
2022-11-19 14:02:03 +0100

    synchronized content with esa repository

--- cmake/spdlog.cmake
+++ cmake/spdlog.cmake
@@ -2,21 +2,33 @@ include(FetchContent)
 
 message(STATUS "Setting up spdlog")
 
-#Fetches the version 1.9.2 for spdlog
-FetchContent_Declare(spdlog
-        GIT_REPOSITORY https://github.com/gabime/spdlog.git
-        GIT_TAG v1.9.2
-        )
+find_package(spdlog 1.11.0 QUIET)
 
-# Disable stuff we don't need
-option(SPDLOG_BUILD_EXAMPLE "" OFF)
-option(SPDLOG_BUILD_TESTS "" OFF)
-option(SPDLOG_INSTALL "" OFF)
+if (${spdlog_FOUND})
 
-FetchContent_MakeAvailable(spdlog)
+    message(STATUS "Using existing spdlog installation")
 
-# Disable warnings from the library target
-target_compile_options(spdlog PRIVATE -w)
-# Disable warnings from included headers
-get_target_property(propval spdlog INTERFACE_INCLUDE_DIRECTORIES)
-target_include_directories(spdlog SYSTEM PUBLIC "${propval}")
+else()
+
+    message(STATUS "Using spdlog from git repository")
+
+    #Fetches the version 1.11.0 for spdlog
+    FetchContent_Declare(spdlog
+            GIT_REPOSITORY https://github.com/gabime/spdlog.git
+            GIT_TAG v1.11.0
+            )
+
+    # Disable stuff we don't need
+    option(SPDLOG_BUILD_EXAMPLE "" OFF)
+    option(SPDLOG_BUILD_TESTS "" OFF)
+    option(SPDLOG_INSTALL "" OFF)
+
+    FetchContent_MakeAvailable(spdlog)
+
+    # Disable warnings from the library target
+    target_compile_options(spdlog PRIVATE -w)
+    # Disable warnings from included headers
+    get_target_property(propval spdlog INTERFACE_INCLUDE_DIRECTORIES)
+    target_include_directories(spdlog SYSTEM PUBLIC "${propval}")
+
+endif()
--- cmake/thrust.cmake
+++ cmake/thrust.cmake
@@ -2,12 +2,7 @@ include(FetchContent)
 
 message(STATUS "Setting up thrust")
 
-# Fetches the version 1.16.0 of the official NVIDIA Thrust repository
-FetchContent_Declare(thrust
-        GIT_REPOSITORY https://github.com/NVIDIA/thrust.git
-        GIT_TAG 1.16.0
-        )
-
+# Set custom variables, policies, etc.
 # Disable stuff not needed
 set(THRUST_ENABLE_HEADER_TESTING "OFF")
 set(THRUST_ENABLE_TESTING "OFF")
@@ -16,4 +11,18 @@ set(THRUST_ENABLE_EXAMPLES "OFF")
 # Set standard CPP Dialect to 17 (default of thrust would be 14)
 set(THRUST_CPP_DIALECT 17)
 
-FetchContent_MakeAvailable(thrust)
+find_package(Thrust 1.16.0 QUIET)
+
+if (${Thrust_FOUND})
+
+    message(STATUS "Using existing thrust installation")
+
+else()
+    message(STATUS "Using thrust from git repository")
+    # Fetches the version 1.16.0 of the official NVIDIA Thrust repository
+    FetchContent_Declare(thrust
+            GIT_REPOSITORY https://github.com/NVIDIA/thrust.git
+            GIT_TAG 1.16.0
+            )
+    FetchContent_MakeAvailable(thrust)
+endif()
